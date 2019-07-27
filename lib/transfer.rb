eb1ffc26a8d51ad1f3d4fa3d153903f52c9f8e32
Amanda King <amapickell@gmail.com>
2019-07-27 19:23:03 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -25,9 +25,11 @@ class Transfer
   end
   
   def reverse_transfer
-    self.sender.deposit (self.amount)
-    self.receiver.balance -= self.amount
-    self.status = "reversed"
+    if self.status == "complete"
+      self.sender.deposit (self.amount)
+      self.receiver.balance -= self.amount
+      self.status = "reversed"
+    end
   end
   
 end
