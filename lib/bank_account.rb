d064e2658bbdc9b6b2895ce61e49712b4f62ea41
Amanda King <amapickell@gmail.com>
2019-07-27 19:01:19 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,27 @@
 class BankAccount
+  attr_accessor :balance, :status
+  attr_reader :name
+
+  def initialize (name)
+    @balance = 1000
+    @status = "open"
+    @name = name
+  end
+  
+  def deposit (amount)
+    self.balance += amount
+  end
+  
+  def display_balance
+    puts @balance
+  end
+  
+  def valid?
+    true if @status == "open" && @balance >= 0
+  end
+  
+  def close_account
+    @status = "closed"
+  end
 
 end
