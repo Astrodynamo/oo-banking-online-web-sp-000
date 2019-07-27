e44d2b9eeed719fbab49a7775079118c796f684c
Amanda King <amapickell@gmail.com>
2019-07-27 19:05:20 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
-  # your code here
+  attr_accessor :status
+  attr_reader :sender, :receiver, :amount
+  
+  def initialize (sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+  
+  def valid?
+    true if self.sender.valid? && self.receiver.valid?
+  end
+  
+  def execute_transaction
+    
+  end
+  
+  def reverse_transfer
+    
+  end
+  
 end
