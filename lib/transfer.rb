82255682ba594975d0b0748819645a2ee7cf314a
Amanda King <amapickell@gmail.com>
2019-07-27 19:19:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -14,7 +14,7 @@ class Transfer
   end
   
   def execute_transaction
-    if self.valid?
+    if self.valid? && self.status == "pending"
       self.sender.balance -= self.amount
       self.receiver.deposit (self.amount)
       self.status = "complete"
